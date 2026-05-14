513a8c0716db98104f811d146ba458269305453b
Oleh Prukhnytskyi <oleh.prukhnytskyi@gmail.com>
2026-05-14 16:43:32 +0100

    Fix kafka consumer

--- src/main/java/com/olehprukhnytskyi/macrotrackerweightservice/consumer/UserEventConsumer.java
+++ src/main/java/com/olehprukhnytskyi/macrotrackerweightservice/consumer/UserEventConsumer.java
@@ -1,7 +1,5 @@
 package com.olehprukhnytskyi.macrotrackerweightservice.consumer;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.olehprukhnytskyi.event.UserDeletedEvent;
 import com.olehprukhnytskyi.exception.EventProcessingException;
 import com.olehprukhnytskyi.exception.error.EventErrorCode;
@@ -16,18 +14,9 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class UserEventConsumer {
     private final WeightService weightService;
-    private final ObjectMapper objectMapper;
 
     @KafkaListener(topics = "user-deleted", groupId = "weight-service")
-    public void handleUserDeleted(String message) {
-        UserDeletedEvent event;
-        try {
-            event = objectMapper.readValue(message, UserDeletedEvent.class);
-        } catch (JsonProcessingException e) {
-            log.error("Invalid user-deleted event payload. Message: {}", message, e);
-            throw new EventProcessingException(EventErrorCode.EVENT_DESERIALIZATION_FAILED,
-                    "Failed to parse user-deleted event", e);
-        }
+    public void handleUserDeleted(UserDeletedEvent event) {
         try {
             log.info("Processing user-deleted event for userId={}", event.getUserId());
             weightService.deleteUserWeightsRecursively(event.getUserId());
